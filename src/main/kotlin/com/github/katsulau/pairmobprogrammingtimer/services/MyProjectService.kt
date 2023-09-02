968c81b26b65a5528d6764a41bcbdfa62ee56041
GitHub Action <action@github.com>
2023-09-02 08:45:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/katsulau/pairmobprogrammingtimer/services/MyProjectService.kt
+++ src/main/kotlin/com/github/katsulau/pairmobprogrammingtimer/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.katsulau.pairmobprogrammingtimer.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.katsulau.pairmobprogrammingtimer.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/katsulau/pairmobprogrammingtimer/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/katsulau/pairmobprogrammingtimer/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.katsulau.pairmobprogrammingtimer.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.katsulau.pairmobprogrammingtimer.MyBundle
+import com.github.katsulau.pairmobprogrammingtimer.services.MyProjectService
 import javax.swing.JButton
 
 
